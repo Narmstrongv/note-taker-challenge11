4b5c8569ebd5a4891a93687a078166d5a16db294
Newton <narmstrongv@gmail.com>
2021-07-04 17:29:18 -0700

    added js

--- server.js
+++ server.js
@@ -13,19 +13,23 @@ app.use(express.json());
 //write app.POST
 //this will write to db.json
 //it will take in the note title & note text
+//take
+//add the ID before push to array
 
+//req is stuff from user
+//res is what you send
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
-app.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/db/db.json'));
 });
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+})
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
